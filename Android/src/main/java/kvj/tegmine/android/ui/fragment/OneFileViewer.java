525ef571a376cb2610dec8c923e52c59a8f79658
Konstantin Vorobyev <vorobyev@coacha.com>
2015-03-17 11:50:53 +0900

    Fix #25: Copy/paste of file parts

--- Android/src/main/java/kvj/tegmine/android/ui/fragment/OneFileViewer.java
+++ Android/src/main/java/kvj/tegmine/android/ui/fragment/OneFileViewer.java
@@ -1,6 +1,10 @@
 package kvj.tegmine.android.ui.fragment;
 
 import android.animation.ObjectAnimator;
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,6 +12,8 @@ import android.text.TextUtils;
 import android.util.TypedValue;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +27,7 @@ import org.kvj.bravo7.form.FormController;
 import org.kvj.bravo7.log.Logger;
 import org.kvj.bravo7.util.Tasks;
 
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -81,7 +88,7 @@ public class OneFileViewer extends Fragment {
         if (null == controller) { // Invalid fragment
             return null;
         }
-        logger.d("Will create view", controller);
+        setHasOptionsMenu(true);
         View view = inflater.inflate(R.layout.fragment_one_file, container, false);
         titleText = (TextView) view.findViewById(R.id.one_file_title_text);
         titleText.setTextSize(TypedValue.COMPLEX_UNIT_SP, controller.theme().headerTextSp());
@@ -100,16 +107,9 @@ public class OneFileViewer extends Fragment {
             }
         });
         listView = (ListView) view.findViewById(android.R.id.list);
-        registerForContextMenu(listView);
         adapter = new OneFileAdapter(controller, item);
         listView.setAdapter(adapter);
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                onLongItemClick(position);
-                return true;
-            }
-        });
+        registerForContextMenu(listView);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -160,6 +160,65 @@ public class OneFileViewer extends Fragment {
         }
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_one_file, menu);
+    }
+
+    private ClipboardManager getClipboard() {
+        return (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+    }
+
+    private void appendText(final CharSequence text, final Runnable afterFinish) {
+        Tasks.SimpleTask<FileSystemException> task = new Tasks.SimpleTask<FileSystemException>() {
+            @Override
+            protected FileSystemException doInBackground() {
+                OutputStream stream = null;
+                try {
+                    stream = controller.fileSystemProvider().append(item);
+                    controller.writeEdited(stream, text.toString().trim(), false);
+                } catch (FileSystemException e) {
+                    return e;
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(FileSystemException e) {
+                logger.d("Save result:", e);
+                if (null == e) { // Saved
+                    refresh();
+                    if (null != afterFinish) afterFinish.run();
+                } else {
+                    logger.e(e, "Fail save error");
+                }
+            }
+        };
+        task.exec();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (null == controller) return true; // Ignore
+        switch (item.getItemId()) {
+            case R.id.menu_paste_text:
+                if (!getClipboard().hasPrimaryClip() || !getClipboard().getPrimaryClipDescription().hasMimeType(
+                    ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+                    SuperActivity.notifyUser(getActivity(), "No text to paste");
+                    return true;
+                }
+                CharSequence text = getClipboard().getPrimaryClip().getItemAt(0).getText();
+                appendText(text, new Runnable() {
+                    @Override
+                    public void run() {
+                        SuperActivity.notifyUser(getActivity(), "Text pasted");
+                    }
+                });
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public void onListItemClick(int position) {
         if (null != adapter) { // Toggle folding
             adapter.toggle(position);
@@ -167,39 +226,34 @@ public class OneFileViewer extends Fragment {
     }
 
     @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        if (null != adapter && v == listView) { // OK to show
-            logger.d("Listview menu:", menuInfo);
-            menu.clear();
-            getActivity().getMenuInflater().inflate(R.menu.context_one_file, menu);
-            final String contents = adapter.partString(listView.getSelectedItemPosition());
-            logger.d("Show menu for:", listView.getSelectedItemPosition(), contents);
-            if (TextUtils.isEmpty(contents)) { // No data
-                return;
-            }
-            menu.findItem(R.id.context_copy_cboard).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-                @Override
-                public boolean onMenuItemClick(MenuItem item) {
-                    // Copy here
-                    return true;
-                }
-            });
-            menu.findItem(R.id.context_share).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-                @Override
-                public boolean onMenuItemClick(MenuItem item) {
-                    Intent sendIntent = new Intent();
-                    sendIntent.setAction(Intent.ACTION_SEND);
-                    sendIntent.putExtra(Intent.EXTRA_TEXT, contents);
-                    sendIntent.setType("text/plain");
-                    startActivity(sendIntent);
-                    return true;
-                }
-            });
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+        final String contents = adapter.partString(info.position);
+        if (TextUtils.isEmpty(contents)) { // No data
+            return true;
         }
+        switch (item.getItemId()) {
+            case R.id.context_share:
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, contents);
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+                return true;
+            case R.id.context_copy_cboard:
+                ClipData clip = ClipData.newPlainText(contents, contents);
+                getClipboard().setPrimaryClip(clip);
+                SuperActivity.notifyUser(getActivity(), "Copied to clipboard");
+        }
+        return false;
     }
 
-    private void onLongItemClick(int position) {
-//        listView.showContextMenu();
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        if (null != adapter && v == listView) { // OK to show
+            menu.clear();
+            getActivity().getMenuInflater().inflate(R.menu.context_one_file, menu);
+        }
     }
 
     private void loadFileLayout() {
